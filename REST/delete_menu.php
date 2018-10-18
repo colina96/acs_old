3ea491fe667203518f2a95432477712816aeeb9e
Col <colin.p.atkinson@gmail.com>
2018-10-18 17:54:57 +1100

    added delete_menu.php

---
+++ REST/delete_menu.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+include '../db.php';
+$con = $GLOBALS['con'];
+$userID = $_SESSION['userID'];
+$menu_id = get_url_token('menu_id');
+// echo "userID ".$userID."\n";
+if ($userID > 0 && $menu_id > 0) {
+
+	$sql = "delete from MENUS ";
+	$sql .= ' where id='.$menu_id;
+	echo $sql;
+	test_mysql_query($sql);
+	$sql = "delete from MENU_ITEMS ";
+	$sql .= ' where menu_id='.$menu_id;
+	test_mysql_query($sql);echo $sql;
+	$sql = "delete from MENU_ITEM_COMPONENTS ";
+	$sql .= ' where menu_id='.$menu_id;
+	test_mysql_query($sql);echo $sql;
+	$sql = "delete from MENU_ITEM_LINK ";
+	$sql .= ' where menu_id='.$menu_id;
+	test_mysql_query($sql);echo $sql;
+}
+ 
+?>
