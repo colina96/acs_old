257c2932b500773eab3f13a799524388e4b5933d
Falk Fairfield <falk.schoenfeld@tum.de>
2019-01-04 13:31:38 +1100

    FIXUP code style

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -22,52 +22,51 @@ var USER = null;
 
 var app = {
     // Application Constructor
-    initialize: function() {
-    	console.log("Application Constructor");
+    initialize: function () {
+        console.log("Application Constructor");
         this.bindEvents();
         try {
-        	var storage = window.localStorage;
-        	if (storage.getItem('RESTHOME')) {
-        		var s = storage.getItem('RESTHOME');
-        		// alert ("got RESTHOME " + storage.getItem('RESTHOME') + "X" + typeof(storage.getItem('RESTHOME')) + "X");
-        		if (s.length < 2){
-        			// alert ("setting RESTHOME to ",RESTHOME);
-            		storage.setItem('RESTHOME',RESTHOME);
+            var storage = window.localStorage;
+            if (storage.getItem('RESTHOME')) {
+                var s = storage.getItem('RESTHOME');
+                // alert ("got RESTHOME " + storage.getItem('RESTHOME') + "X" + typeof(storage.getItem('RESTHOME')) + "X");
+                if (s.length < 2) {
+                    // alert ("setting RESTHOME to ",RESTHOME);
+                    storage.setItem('RESTHOME', RESTHOME);
                 }
-        	}
-        	//else {
-        		storage.setItem('RESTHOME',RESTHOME);
-        	// }
-        }
-        catch(e) {
-        	alert("ERROR  " + e);
+            }
+            //else {
+            storage.setItem('RESTHOME', RESTHOME);
+            // }
+        } catch (e) {
+            alert("ERROR  " + e);
         }
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-    	console.log("onDeviceReady");
-    	log("starting");
-    	//RESTHOME = "http://www.evoz.com.au/acs/REST/";
-    	RESTHOME = "http://10.0.0.32/acs/REST/";
-    	// RESTHOME = "http://192.168.0.52/acs/REST/";
-    	// document.getElementById('login_btn').innerHTML = 'Login to www.evoz.com.au';
-    	check_login();
-    	qpack_init();
+    onDeviceReady: function () {
+        console.log("onDeviceReady");
+        log("starting");
+        //RESTHOME = "http://www.evoz.com.au/acs/REST/";
+        RESTHOME = "http://10.0.0.32/acs/REST/";
+        // RESTHOME = "http://192.168.0.52/acs/REST/";
+        // document.getElementById('login_btn').innerHTML = 'Login to www.evoz.com.au';
+        check_login();
+        qpack_init();
         app.receivedEvent('deviceready');
     },
- 
+
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -79,220 +78,198 @@ var app = {
     }
 };
 
-function setup_front_page()
-{
-	if (!USER) return;
-	console.log('setup_front_page');
-	console.log(USER);
-	// this is really ugle but bootstrap breaks everything else..... TODO fix
-	var div = document.getElementById('front_page_btns');
-	var innerHTML = '';
-	if (USER.dock && USER.dock == 1)
-		innerHTML += "<button type='button' class='m_btn' onclick='goto_dock();'>Dock</button>";
-	if (USER.kitchen && USER.kitchen == 1)
-		innerHTML += "<button id='kit_btn' type='button' class='m_btn' onclick=\"goto_m_main('kitchen');\">Kitchen</button>";
-	if (USER.plating && USER.plating == 1)
-		innerHTML += "<button id='plat_btn' type='button'  class='m_btn' onclick=\"goto_plating_teams();\">Plating</button>";
-	innerHTML += "<button id='out_btn' type='button'  class='m_btn' onclick=\"logout();\">Logout</button>";
-	div.innerHTML = innerHTML;
+function setup_front_page() {
+    if (!USER) return;
+    console.log('setup_front_page');
+    console.log(USER);
+    // this is really ugle but bootstrap breaks everything else..... TODO fix
+    var div = document.getElementById('front_page_btns');
+    var innerHTML = '';
+    if (USER.dock && USER.dock == 1)
+        innerHTML += "<button type='button' class='m_btn' onclick='goto_dock();'>Dock</button>";
+    if (USER.kitchen && USER.kitchen == 1)
+        innerHTML += "<button id='kit_btn' type='button' class='m_btn' onclick=\"goto_m_main('kitchen');\">Kitchen</button>";
+    if (USER.plating && USER.plating == 1)
+        innerHTML += "<button id='plat_btn' type='button'  class='m_btn' onclick=\"goto_plating_teams();\">Plating</button>";
+    innerHTML += "<button id='out_btn' type='button'  class='m_btn' onclick=\"logout();\">Logout</button>";
+    div.innerHTML = innerHTML;
 }
 
-function check_login()
-{
-var loginString ="";
-console.log("checking login");
-$("#login_fail").text("Checking login ..!" + RESTHOME);
-$.ajax({
-    type: "POST",crossDomain: true, cache: false,
-    url:  RESTHOME + "login.php",
-    data: loginString,
-    dataType: 'json',
-    success: function(data){
-    	console.log("got login");
-    	console.log(data);
-    	//document.getElementById("res").innerHTML = "Logout Success..!" + data;
-    	if(data['user_id']) {
-        	//document.getElementById("res").innerHTML = "Login ID" + data['user_id'];
-        	user_id = data['user_id'];
-        	user_name = data['user'];
-        	USER = data['USER'];
-        	
-        	if (user_id <= 0) {
-        		// load_comps();
-            	// load_preptypes();
-        		barcode_mode = 'login';
-        		openPage('login_div', this, 'red','mobile_main','tabclass');
-        		$("#login_fail").text("Not logged in ..!");
-        	}
-        	else {
-        		setup_front_page();
-            $("#login_fail").text("Login OK..!");
-            	console.log("Login OK");
-        		load_comps();
-            	load_preptypes();
-        		user_name = data['user'];
-        		document.getElementById('m_login').innerHTML = user_name;
-        		openPage('mm1', this, 'red','mobile_main','tabclass');
-        	}
-           // localStorage.loginstatus = "true";
-           //  window.location.href = "welcome.html";
-        }
-        else if(data == "error")
-        {
-            $("#login_fail").text("Login Failed..!");
-        }
-    }
-});
-}
-
-function Xcheck_login()
-{
-	if (user_id <= 0) {
-		openPage('login_div', this, 'red','mobile_main','tabclass');
-	}
-	else {
-		openPage('mm1', this, 'red','mobile_main','tabclass');
-		
-	}
-}
-
-function set_admin()
-{
-	// document.getElementById('log_div').style.display = 'block';
-	// goto_home();
-	openPage('setup', this, 'red','mobile_main','tabclass');
-	document.getElementsByName('resthome')[0].value = RESTHOME;
-}
-
-function save_resthome()
-{
-	RESTHOME = document.getElementsByName('resthome')[0].value;
-	var storage = window.localStorage;
-	
-    storage.setItem('RESTHOME',RESTHOME);
-}
-
-function exit_app()
-{
-	KioskPlugin.exitKiosk();
-}
-
-function login(barcode_uid)
-{
-    //var email= "colin.p.atkinson@gmail.com";
-    //var password= "acs";
-	
-	var loginString = null;
-	if (barcode_uid > 0) {
-		loginString="uid="+barcode_uid+"&login=login";
-	}
-	else {
-		var email = document.getElementsByName('email')[0].value.toLowerCase();
-		var password = document.getElementsByName('password')[0].value.toLowerCase();
-    //$("#status").text("Authenticating...");
-		
-		loginString ="email="+email+"&password="+password+"&login=login";
-		console.log("Authenticating...",RESTHOME,loginString);
-		if (email == 'admin' && password == "zzz") {
-			set_admin();
-			return;
-		}
-	}
-	qpack_start();
-    console.log(loginString);
-    
+function check_login() {
+    var loginString = "";
+    console.log("checking login");
+    $("#login_fail").text("Checking login ..!" + RESTHOME);
     $.ajax({
-        type: "POST",crossDomain: true, cache: false,
-        url:  RESTHOME + "login.php",
+        type: "POST", crossDomain: true, cache: false,
+        url: RESTHOME + "login.php",
         data: loginString,
         dataType: 'json',
-        success: function(data){
-        	document.getElementById("login_fail").innerHTML = "2 Login Success..!" + data['user_id'];
-        	console.log("Authenticated |",data,'|');
-            if(data['user_id']) {
-           	document.getElementById("login_fail").innerHTML = "2 Login ID" + data['user_id'];
-            	user_id = data['user_id'];
-            	user_name = data['user'];
-            	USER = data['USER'];
-            	if (user_id > 0) {
-            		set_info('OK');
-               		load_comps();
-                	load_preptypes();
-            		user_name = data['user'];
-            		document.getElementById('m_login').innerHTML = user_name;
-            		setup_front_page();
-            		goto_home();
-            	}
-            	else {
-            		set_info('INVALID USER');
-            		document.getElementById('login_fail').innerHTML = "login fail for "+email + " " + password;
-            		
-            	}
-            }
-            else 
-            {
-            	$("#login_fail").text("login failed unknown error");
+        success: function (data) {
+            console.log("got login");
+            console.log(data);
+            //document.getElementById("res").innerHTML = "Logout Success..!" + data;
+            if (data['user_id']) {
+                //document.getElementById("res").innerHTML = "Login ID" + data['user_id'];
+                user_id = data['user_id'];
+                user_name = data['user'];
+                USER = data['USER'];
+
+                if (user_id <= 0) {
+                    // load_comps();
+                    // load_preptypes();
+                    barcode_mode = 'login';
+                    openPage('login_div', this, 'red', 'mobile_main', 'tabclass');
+                    $("#login_fail").text("Not logged in ..!");
+                } else {
+                    setup_front_page();
+                    $("#login_fail").text("Login OK..!");
+                    console.log("Login OK");
+                    load_comps();
+                    load_preptypes();
+                    user_name = data['user'];
+                    document.getElementById('m_login').innerHTML = user_name;
+                    openPage('mm1', this, 'red', 'mobile_main', 'tabclass');
+                }
+                // localStorage.loginstatus = "true";
+                //  window.location.href = "welcome.html";
+            } else if (data == "error") {
+                $("#login_fail").text("Login Failed..!");
             }
         }
     });
 }
 
-function logout()
-{
-    var loginString ="logout=login";
-    console.log('logging out');
-    set_info('');
-    set_barcode_mode ('login');
-    openPage('login_div', this, 'red','mobile_main','tabclass');
-    openPage('login_div2', this, 'red','m_modal2','tabclass');
+function Xcheck_login() {
+    if (user_id <= 0) {
+        openPage('login_div', this, 'red', 'mobile_main', 'tabclass');
+    } else {
+        openPage('mm1', this, 'red', 'mobile_main', 'tabclass');
+    }
+}
+
+function set_admin() {
+    // document.getElementById('log_div').style.display = 'block';
+    // goto_home();
+    openPage('setup', this, 'red', 'mobile_main', 'tabclass');
+    document.getElementsByName('resthome')[0].value = RESTHOME;
+}
+
+function save_resthome() {
+    RESTHOME = document.getElementsByName('resthome')[0].value;
+    var storage = window.localStorage;
+
+    storage.setItem('RESTHOME', RESTHOME);
+}
+
+function exit_app() {
+    KioskPlugin.exitKiosk();
+}
+
+function login(barcode_uid) {
+    //var email= "colin.p.atkinson@gmail.com";
+    //var password= "acs";
+
+    var loginString = null;
+    if (barcode_uid > 0) {
+        loginString = "uid=" + barcode_uid + "&login=login";
+    } else {
+        var email = document.getElementsByName('email')[0].value.toLowerCase();
+        var password = document.getElementsByName('password')[0].value.toLowerCase();
+        //$("#status").text("Authenticating...");
+
+        loginString = "email=" + email + "&password=" + password + "&login=login";
+        console.log("Authenticating...", RESTHOME, loginString);
+        if (email == 'admin' && password == "zzz") {
+            set_admin();
+            return;
+        }
+    }
+    qpack_start();
+    console.log(loginString);
+
     $.ajax({
-        type: "POST",crossDomain: true, cache: false,
-        url:  RESTHOME + "login.php",
+        type: "POST", crossDomain: true, cache: false,
+        url: RESTHOME + "login.php",
         data: loginString,
         dataType: 'json',
-        success: function(data){
-        	console.log('logout got',data);
-        	if(data['user_id']) {
-            	user_id = data['user_id'];
-            	if (user_id > 0) {
-            		openPage('mm2', this, 'red','mobile_main','tabclass');
-            	}
-            	else {
-            		openPage('login_div', this, 'red','mobile_main','tabclass');
-            	}
-               // localStorage.loginstatus = "true";
-               //  window.location.href = "welcome.html";
+        success: function (data) {
+            document.getElementById("login_fail").innerHTML = "2 Login Success..!" + data['user_id'];
+            console.log("Authenticated |", data, '|');
+            if (data['user_id']) {
+                document.getElementById("login_fail").innerHTML = "2 Login ID" + data['user_id'];
+                user_id = data['user_id'];
+                user_name = data['user'];
+                USER = data['USER'];
+                if (user_id > 0) {
+                    set_info('OK');
+                    load_comps();
+                    load_preptypes();
+                    user_name = data['user'];
+                    document.getElementById('m_login').innerHTML = user_name;
+                    setup_front_page();
+                    goto_home();
+                } else {
+                    set_info('INVALID USER');
+                    document.getElementById('login_fail').innerHTML = "login fail for " + email + " " + password;
+
+                }
+            } else {
+                $("#login_fail").text("login failed unknown error");
             }
-            else if(data == "error")
-            {
+        }
+    });
+}
+
+function logout() {
+    var loginString = "logout=login";
+    console.log('logging out');
+    set_info('');
+    set_barcode_mode('login');
+    openPage('login_div', this, 'red', 'mobile_main', 'tabclass');
+    openPage('login_div2', this, 'red', 'm_modal2', 'tabclass');
+    $.ajax({
+        type: "POST", crossDomain: true, cache: false,
+        url: RESTHOME + "login.php",
+        data: loginString,
+        dataType: 'json',
+        success: function (data) {
+            console.log('logout got', data);
+            if (data['user_id']) {
+                user_id = data['user_id'];
+                if (user_id > 0) {
+                    openPage('mm2', this, 'red', 'mobile_main', 'tabclass');
+                } else {
+                    openPage('login_div', this, 'red', 'mobile_main', 'tabclass');
+                }
+                // localStorage.loginstatus = "true";
+                //  window.location.href = "welcome.html";
+            } else if (data == "error") {
                 //$("#status").text("Login Failed..!");
             }
         }
     });
 }
 
-function openPage(pageName, elmnt, color,content_class,tab_class,callback) 
-{
-	var popups = document.getElementsByClassName('popup');
+function openPage(pageName, elmnt, color, content_class, tab_class, callback) {
+    var popups = document.getElementsByClassName('popup');
     for (i = 0; i < popups.length; i++) {
-    		popups[i].style.display = "none";
-    } 
+        popups[i].style.display = "none";
+    }
 //	var parent = document.getElementById(pageName).parentElement;
 //	console.log("opening page ",pageName,content_class);
 //	console.log('parent ',parent.id,parent.className);
-	var d = document.getElementById(pageName);
-	console.log(d);
-	// console.log(elmnt.id);
+    var d = document.getElementById(pageName);
+    console.log(d);
+    // console.log(elmnt.id);
     // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName(content_class);
     for (i = 0; i < tabcontent.length; i++) {
-    	// console.log("found tab ",tabcontent[i].id);
-    	try {
-    		tabcontent[i].style.display = "none";
-    	}
-        catch (e) {
-        	console.log("who knows.....");
+        // console.log("found tab ",tabcontent[i].id);
+        try {
+            tabcontent[i].style.display = "none";
+        } catch (e) {
+            console.log("who knows.....");
         }
     }
 
@@ -300,16 +277,16 @@ function openPage(pageName, elmnt, color,content_class,tab_class,callback)
     tablinks = document.getElementsByClassName(tab_class);
     // console.log("found tablinks ",tablinks.length);
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.borderBottom ="";
-    } 
+        tablinks[i].style.borderBottom = "";
+    }
 
     // Show the specific tab content
     document.getElementById(pageName).style.display = "block";
     if (elmnt && elmnt.style) {
-    	// document.getElementById(elmnt.id).style.borderBottom = '1px solid white';
-    	elmnt.style.borderBottom = '1px solid white';
+        // document.getElementById(elmnt.id).style.borderBottom = '1px solid white';
+        elmnt.style.borderBottom = '1px solid white';
     }
     // Add the specific color to the button used to open the tab content
     // elmnt.style.backgroundColor = color;
-    if (typeof(callback) == 'function' ) callback();
+    if (typeof (callback) == 'function') callback();
 }
