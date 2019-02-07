794fb9a26cd0f3f7a2c1494f112be30234472cf4
Falk Fairfield <falk.schoenfeld@tum.de>
2019-02-07 13:51:56 +1100

    change ip to tsl

--- app/www/js/server-config.js
+++ app/www/js/server-config.js
@@ -1,3 +1,4 @@
-var SERVER_URL="http://192.168.0.52";
+// var SERVER_URL="http://192.168.0.52";
+var SERVER_URL="http://10.0.28.119";
 // var SERVER_URL="http://10.0.0.32/";
 // var SERVER_URL="http://www.evoz.com.au/";
