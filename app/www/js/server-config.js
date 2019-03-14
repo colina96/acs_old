2bac7bff9ae633cec64ced18e451999ea437a072
Andrew Asfaganov <andrew.asfaganov@gmail.com>
2019-03-14 13:28:24 +1100

    readded server-config.js

---
+++ app/www/js/server-config.js
@@ -0,0 +1,5 @@
+// var SERVER_URL="http://192.168.0.52";
+//var SERVER_URL="http://10.0.28.119";
+// var SERVER_URL="http://10.0.0.32/";
+// var SERVER_URL="http://www.evoz.com.au/";
+var SERVER_URL="http://10.0.30.69";
